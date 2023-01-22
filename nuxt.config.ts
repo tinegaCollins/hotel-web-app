7e448243dfc0d968d4c09b32955f4e66cad95aa1
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-22 22:34:39 +0300

    page transition

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,25 @@ import presetIcons from '@unocss/preset-icons'
 
 // eslint-disable-next-line no-undef
 export default defineNuxtConfig({
-    css: ['~/assets/css/tailwind.css'],
+    css: ["~/assets/css/tailwind.css", "~/assets/css/global.css"],
     buildModules: ['@nuxtjs/tailwindcss', '@unocss/nuxt', '@nuxtjs/color-mode',"nuxt-icon"],
     colorMode: {
         classSuffix: '',
     },
     app: {
         head: {
-            charset: 'utf-16',
-            viewport: 'width=500, initial-scale=1',
-            title: 'Print-Depot',
-            meta: [{ name: 'description', content: 'Print Depot site' }],
-        },
+            title: 'Modern chefs',
+            meta: [
+              { charset: 'utf-8' },
+              { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+              {
+                hid: 'chefs',
+                name: 'chefs',
+                content: 'modern Chefs'
+              }
+            ],
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+          },
         pageTransition: { name: 'page', mode: 'out-in' },
     },
     unocss: {
