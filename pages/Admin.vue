ae63a6acde974f807bd4acc4c24af45ae86c3664
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-22 22:06:42 +0300

    admin-bg to default

--- pages/Admin.vue
+++ pages/Admin.vue
@@ -1,6 +1,6 @@
 <template>
-  <main class="min-h-screen bg-slate-700">
-    <div class="container p-2 mx-auto sm:p-4 dark:text-gray-100">
+  <main class="">
+    <div class="container p-2 mx-auto sm:p-4 text-gray-800">
       <h2 class="mb-4 text-2xl font-semibold leading-tight">Invoices</h2>
       <div class="overflow-x-auto">
         <table class="min-w-full text-xs">
@@ -12,7 +12,7 @@
             <col />
             <col class="w-24" />
           </colgroup>
-          <thead class="dark:bg-gray-700">
+          <thead class="bg-gray-100">
             <tr class="text-left">
               <th class="p-3">Invoice #</th>
               <th class="p-3">Client</th>
@@ -23,7 +23,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr class="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+            <tr class="border-b border-opacity-20 bg-gray-100">
               <td class="p-3">
                 <p>97412378923</p>
               </td>
@@ -32,24 +32,24 @@
               </td>
               <td class="p-3">
                 <p>14 Jan 2022</p>
-                <p class="dark:text-gray-400">Friday</p>
+                <p class="text-gray-700">Friday</p>
               </td>
               <td class="p-3">
                 <p>01 Feb 2022</p>
-                <p class="dark:text-gray-400">Tuesday</p>
+                <p class="text-gray-700">Tuesday</p>
               </td>
               <td class="p-3 text-right">
                 <p>$15,792</p>
               </td>
               <td class="p-3 text-right">
                 <span
-                  class="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900"
+                  class="px-3 py-1 font-semibold rounded-md bg-violet-400 text-gray-900"
                 >
                   <span>Pending</span>
                 </span>
               </td>
             </tr>
-            <tr class="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+            <tr class="border-b border-opacity-20 border-gray-700 bg-gray-100">
               <td class="p-3">
                 <p>97412378923</p>
               </td>
@@ -58,24 +58,24 @@
               </td>
               <td class="p-3">
                 <p>14 Jan 2022</p>
-                <p class="dark:text-gray-400">Friday</p>
+                <p class="text-gray-700">Friday</p>
               </td>
               <td class="p-3">
                 <p>01 Feb 2022</p>
-                <p class="dark:text-gray-400">Tuesday</p>
+                <p class="text-gray-700">Tuesday</p>
               </td>
               <td class="p-3 text-right">
                 <p>$275</p>
               </td>
               <td class="p-3 text-right">
                 <span
-                  class="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900"
+                  class="px-3 py-1 font-semibold rounded-md text-gray-800 dark:bg-violet-400"
                 >
                   <span>Pending</span>
                 </span>
               </td>
             </tr>
-            <tr class="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+            <tr class="border-b border-opacity-20 border-gray-700 dark:bg-gray-100">
               <td class="p-3">
                 <p>97412378923</p>
               </td>
@@ -84,24 +84,24 @@
               </td>
               <td class="p-3">
                 <p>14 Jan 2022</p>
-                <p class="dark:text-gray-400">Friday</p>
+                <p class="text-gray-700">Friday</p>
               </td>
               <td class="p-3">
                 <p>01 Feb 2022</p>
-                <p class="dark:text-gray-400">Tuesday</p>
+                <p class="text-gray-700">Tuesday</p>
               </td>
               <td class="p-3 text-right">
                 <p>$8,950,500</p>
               </td>
               <td class="p-3 text-right">
                 <span
-                  class="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900"
+                  class="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 text-gray-700"
                 >
                   <span>Pending</span>
                 </span>
               </td>
             </tr>
-            <tr class="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+            <tr class="border-b border-opacity-20 border-gray-700 dark:bg-gray-100">
               <td class="p-3">
                 <p>97412378923</p>
               </td>
@@ -110,18 +110,18 @@
               </td>
               <td class="p-3">
                 <p>14 Jan 2022</p>
-                <p class="dark:text-gray-400">Friday</p>
+                <p class="text-gray-700">Friday</p>
               </td>
               <td class="p-3">
                 <p>01 Feb 2022</p>
-                <p class="dark:text-gray-400">Tuesday</p>
+                <p class="text-gray-700">Tuesday</p>
               </td>
               <td class="p-3 text-right">
                 <p>$98,218</p>
               </td>
               <td class="p-3 text-right">
                 <span
-                  class="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900"
+                  class="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 text-gray-700"
                 >
                   <span>Pending</span>
                 </span>
